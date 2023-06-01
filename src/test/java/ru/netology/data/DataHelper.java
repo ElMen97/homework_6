beb46e517f472496f7017428627964a8003542c3
Elvira Menshikova <em97-97@mail.ru>
2023-06-01 22:14:57 +0400

    src

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,39 +1,69 @@
-package ru.netology.page;
+package ru.netology.data;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
+import lombok.Value;
 
-import ru.netology.data.DataHelper;
-
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Objects;
 
-public class LoginPage {
-    private SelenideElement loginField = $("[data-test-id=login] input");
-    private SelenideElement passwordField = $("[data-test-id=password] input");
-    private SelenideElement loginButton = $("[data-test-id=action-login]");
-    private SelenideElement errorMassage = $("[data-test-id='error-notification']");
-
-    public void getErrorMassage(String textError) {
-        errorMassage
-                .shouldHave(Condition.text(textError))
-                .shouldBe(visible);
+public class DataHelper {
+    private DataHelper() {
     }
 
-    public VerificationPage validLogin(DataHelper.AuthInfo info) {
-        loginField.setValue(info.getLogin());
-        passwordField.setValue(info.getPassword());
-        loginButton.click();
-        return new VerificationPage();
+    private static Faker faker;
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
     }
 
-    public void invalidLoginOrPassword(DataHelper.AuthInfo info) {
-        loginField.setValue(info.getLogin());
-        passwordField.setValue(info.getPassword());
-        loginButton.click();
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static AuthInfo getOtherAuthInfo() {
+        return new AuthInfo("petya", "123qwerty");
+    }
+
+    public static AuthInfo getAuthInfoWithOtherLogin() {
+        faker = new Faker(new Locale("en"));
+        return new AuthInfo(faker.name().username(), "123qwerty");
+    }
+
+    public static AuthInfo getAuthInfoWithOtherPassword() {
+        faker = new Faker(new Locale("en"));
+        return new AuthInfo("vasya", faker.internet().password());
+    }
+
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+
+    public static VerificationCode getNotVerificationCodeFor() {
+        return new VerificationCode("12348");
+    }
+
+    @Value
+    public static class CardData {
+        private String cardNumber;
+    }
+
+    public static String cardNumber(String id) {
+        var city = Arrays.asList("5559000000000001", "5559000000000002");
+        if (Objects.equals(id, "0001")) {
+            return city.get(1);
+        } else {
+            return city.get(0);
+        }
     }
 
 }
--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,25 +1,39 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+
+import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.$;
+
+import java.util.Locale;
 
 public class LoginPage {
-    private SelenideElement loginInput = $x("//span[@data-test-id='login']//input");
-    private SelenideElement passwordInput = $x("//span[@data-test-id='password']//input");
-    private SelenideElement loginButton = $x("//button[@data-test-id='action-login']");
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    private SelenideElement errorMassage = $("[data-test-id='error-notification']");
 
-    public LoginPage() {
-        loginInput.should(visible);
-        passwordInput.should(visible);
-        loginButton.should(visible);
+    public void getErrorMassage(String textError) {
+        errorMassage
+                .shouldHave(Condition.text(textError))
+                .shouldBe(visible);
     }
 
-    public VerificationPage login(UserInfo user) {
-        loginInput.val(user.getLogin());
-        passwordInput.val(user.getPassword());
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
         loginButton.click();
         return new VerificationPage();
     }
+
+    public void invalidLoginOrPassword(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+    }
+
 }
